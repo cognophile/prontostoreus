a50f8d0896d252aaa7f460aad40bcd84776d945f
cognophile <cognophile@users.noreply.github.com>
2018-05-23 22:17:11 +0100

    Add methods for rendering API responses

--- src/Controller/CycleHydrationTrait.php
+++ src/Controller/CycleHydrationTrait.php
@@ -3,6 +3,7 @@
 namespace Prontostoreus\Api\Controller;
 
 use Cake\Event\Event;
+use Cake\Filesystem\Folder;
 
 trait CycleHydrationTrait
 {
@@ -10,6 +11,31 @@ trait CycleHydrationTrait
     {
         // Ensure we render only JSON responses
         $this->RequestHandler->renderAs($this, 'json');
+
         $this->set('_serialize', true);
+    }
+
+    public function respondSuccess(array $data, string $message = "", string $error = "")
+    {
+        $response = [
+            'message' => $message,
+            'success' => true,
+            'error' => "",
+            'data' => $data
+        ];
+    
+        $this->set($response);
+    }
+
+    public function respondError(string $error, string $message = "", array $data = [])
+    {
+        $response = [
+            'message' => $message,
+            'success' => false,
+            'error' => $error,
+            'data' => $data
+        ];
+
+        $this->set($response);
     }
 }
