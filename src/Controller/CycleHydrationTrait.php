7b8a1acf2a73e5f35fc29c475cd291d35de9734d
cognophile <cognophile@users.noreply.github.com>
2018-05-29 01:28:57 +0100

    Add CORs workaround for  local client development

--- src/Controller/CycleHydrationTrait.php
+++ src/Controller/CycleHydrationTrait.php
@@ -12,7 +12,16 @@ trait CycleHydrationTrait
         // Ensure we render only JSON responses
         $this->RequestHandler->renderAs($this, 'json');
 
+        $this->setCorsHeaders();        
         $this->set('_serialize', true);
+    }
+
+    public function beforeFilter(Event $event)
+    {
+        if ($this->request->is('options')) {
+            $this->setCorsHeaders();
+            return $this->response;
+        }
     }
 
     public function respondSuccess(array $data, string $message = "", string $error = "")
@@ -37,5 +46,16 @@ trait CycleHydrationTrait
         ];
 
         $this->set($response);
+    }
+
+    private function setCorsHeaders() 
+    {
+        $this->response->cors($this->request)
+            ->allowOrigin(['http://localhost:8080/'])
+            ->allowMethods(['*'])
+            ->allowHeaders(['Access-Control-Allow-Origin'])
+            ->build();
+
+        $this->response->header('Access-Control-Allow-Origin','*');
     }
 }
