849405bea516dfb113c83cb3658d025c4ca4e265
cognophile <cognophile@users.noreply.github.com>
2018-06-03 14:02:38 +0100

    Add new component scaffold

---
+++ components/CustomerDetailsComponent/src/Controller/AppController.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace CustomerDetailsComponent\Controller;
+
+use Prontostoreus\Api\Controller\AppController as BaseController;
+
+class AppController extends BaseController
+{
+
+}
---
+++ components/CustomerDetailsComponent/src/Plugin.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace CustomerDetailsComponent;
+
+use Cake\Core\BasePlugin;
+
+/**
+ * Plugin for CustomerDetailsComponent
+ */
+class Plugin extends BasePlugin
+{
+}
