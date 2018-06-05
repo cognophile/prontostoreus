0761b0f6371081306a0bff0c58b13d0d92703d53
cognophile <cognophile@users.noreply.github.com>
2018-06-05 22:37:36 +0100

    Add specific add method for requests

--- components/CustomerDetailsComponent/src/Controller/ComponentController.php
+++ components/CustomerDetailsComponent/src/Controller/ComponentController.php
@@ -9,11 +9,33 @@ class ComponentController extends CycleController
     public function initialize() 
     {
         parent::initialize();
+        $this->loadModel('CustomerDetailsComponents.Customers');
+        $this->loadModel('CustomerDetailsComponents.Addresses');
     }
 
     public function status()
     {
         $message = $this->messageHandler->retrieve("General", "RouteAlive");
         $this->respondSuccess([], "Customer details base: {$message}");
+    }
+
+    public function add() 
+    {
+        parent::universalAdd($this->Customers);
+    }
+
+    public function view() 
+    {
+        
+    }
+
+    public function edit() 
+    {
+        
+    }
+
+    public function remove() 
+    {
+        
     }
 }
