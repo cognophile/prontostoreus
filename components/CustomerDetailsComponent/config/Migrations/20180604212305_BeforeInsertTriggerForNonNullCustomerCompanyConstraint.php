5e68a673ff0c2ac75c1969e04793e9d9cabe5d96
cognophile <cognophile@users.noreply.github.com>
2018-06-08 19:05:05 +0100

    Update to signify the action being called

--- components/CustomerDetailsComponent/config/Migrations/20180604212305_BeforeInsertTriggerForNonNullCustomerCompanyConstraint.php
+++ components/CustomerDetailsComponent/config/Migrations/20180604212305_BeforeInsertTriggerForNonNullCustomerCompanyConstraint.php
@@ -19,11 +19,11 @@ class BeforeInsertTriggerForNonNullCustomerCompanyConstraint extends AbstractMig
                 BEGIN
                     IF (NEW.company_id IS NULL AND NEW.customer_id IS NULL) THEN
                         SIGNAL SQLSTATE '45000'
-                        SET MESSAGE_TEXT = 'An address must be related to a company OR a customer';
+                        SET MESSAGE_TEXT = 'INSERT: An address must be related to a company OR a customer';
                     END IF;
                     IF (NEW.company_id IS NOT NULL AND NEW.customer_id IS NOT NULL) THEN
                         SIGNAL SQLSTATE '45000'
-                        SET MESSAGE_TEXT = 'An address cannot be linked to a company AND a customer';
+                        SET MESSAGE_TEXT = 'INSERT: An address cannot be linked to a company AND a customer';
                     END IF;
                 END"
             );
--- components/CustomerDetailsComponent/config/Migrations/20180604213708_BeforeUpdateTriggerForNonNullCustomerCompanyConstraint.php
+++ components/CustomerDetailsComponent/config/Migrations/20180604213708_BeforeUpdateTriggerForNonNullCustomerCompanyConstraint.php
@@ -19,11 +19,11 @@ class BeforeUpdateTriggerForNonNullCustomerCompanyConstraint extends AbstractMig
                 BEGIN
                     IF (NEW.company_id IS NULL AND NEW.customer_id IS NULL) THEN
                         SIGNAL SQLSTATE '45000'
-                        SET MESSAGE_TEXT = 'An address must be related to a company OR a customer';
+                        SET MESSAGE_TEXT = 'UPDATE: An address must be related to a company OR a customer';
                     END IF;
                     IF (NEW.company_id IS NOT NULL AND NEW.customer_id IS NOT NULL) THEN
                         SIGNAL SQLSTATE '45000'
-                        SET MESSAGE_TEXT = 'An address cannot be linked to a company AND a customer';
+                        SET MESSAGE_TEXT = 'UPDATE: An address cannot be linked to a company AND a customer';
                     END IF;
                 END"
             );
