254cf9efb285798a0171209a2e5abebf2d70c8ed
cognophile <cognophile@users.noreply.github.com>
2018-06-25 23:18:11 +0100

    Add routes for room and subsequent furnishing listing

--- components/ApplicationComponent/config/routes.php
+++ components/ApplicationComponent/config/routes.php
@@ -10,13 +10,23 @@ Router::plugin(
         // TODO: Update all component routes to restful resources?
         $routes->get('/', ['controller' => 'Applications', 'action' => 'status']);
 
-        $routes->get('/company/:company_id', ['controller' => 'Applications', 'action' => 'listFurnishings'])
-            ->setPass(['company_id', 'companyId']);
+        // Rooms
+        $routes->get('/room', ['controller' => 'Applications', 'action' => 'fetchRoomList']);
+        $routes->get('/room/:room_id', ['controller' => 'Applications', 'action' => 'fetchRoomList'])
+            ->setPass(['room_id', 'roomId']);
+
+        // Furnishings
+
+        $routes->get('/room/:room_id/furnishing', ['controller' => 'Applications', 'action' => 'fetchFurnishingListByRoom'])
+            ->setPass(['room_id', 'roomId']);
+
+        // $routes->get('/company/:company_id', ['controller' => 'Applications', 'action' => 'listFurnishings'])
+        //     ->setPass(['company_id', 'companyId']);
         
-        $routes->get('/company/:company_id/customer/:customer_id', ['controller' => 'Applications', 'action' => 'view'])
-            ->setPass(['company_id', 'companyId'], ['customer_id', 'customerId']);
+        // $routes->get('/company/:company_id/customer/:customer_id', ['controller' => 'Applications', 'action' => 'view'])
+        //     ->setPass(['company_id', 'companyId'], ['customer_id', 'customerId']);
         
-        $routes->post('/', ['controller' => 'Applications', 'action' => 'add']);
+        // $routes->post('/', ['controller' => 'Applications', 'action' => 'add']);
         // $routes->fallbacks(DashedRoute::class);
     }
 );
