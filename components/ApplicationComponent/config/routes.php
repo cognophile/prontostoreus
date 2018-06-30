a66578ff72622661e80f2e9ce532f48c64423024
cognophile <cognophile@users.noreply.github.com>
2018-06-30 01:49:04 +0100

    Add routes for application creation and update

--- components/ApplicationComponent/config/routes.php
+++ components/ApplicationComponent/config/routes.php
@@ -5,33 +5,33 @@ use Cake\Routing\Route\DashedRoute;
 
 Router::plugin(
     'ApplicationComponent',
-    ['path' => '/apply'],
+    ['path' => '/applications'],
     function (RouteBuilder $routes) {
-        // TODO: Update all component routes to restful resources?
+        // TODO: Update all component routes to restful resources with OPTIONS pre-flight check and p-style before methods
         $routes->get('/', ['controller' => 'Applications', 'action' => 'status']);
 
         // Rooms
         $routes->get('/room', ['controller' => 'Rooms', 'action' => 'fetchRoomList']);
-        $routes->get('/room/:room_id', ['controller' => 'Rooms', 'action' => 'fetchRoomList'])
+        $routes->get('/room/:room_id/', ['controller' => 'Rooms', 'action' => 'fetchRoomList'])
             ->setPass(['room_id']);
 
         // Furnishings
-        $routes->get('/room/:room_id/furnishing', ['controller' => 'Furnishings', 'action' => 'fetchFurnishingListByRoom'])
+        $routes->get('/room/:room_id/furnishing/', ['controller' => 'Furnishings', 'action' => 'fetchFurnishingListByRoom'])
             ->setPass(['room_id']);
 
         $routes->get('/room/:room_id/furnishing/:furnishing_id/', ['controller' => 'Furnishings', 'action' => 'fetchFurnishingSize'])
             ->setPass(['room_id', 'furnishing_id']);
 
+        // CompanyFurnishingRates
         $routes->get('/company/:company_id/furnishing/:furnishing_id/', ['controller' => 'CompanyFurnishingRates', 'action' => 'fetchPriceByItem'])
             ->setPass(['company_id', 'furnishing_id']);
 
-        // $routes->get('/company/:company_id', ['controller' => 'Applications', 'action' => 'listFurnishings'])
-        //     ->setPass(['company_id', 'companyId']);
-        
-        // $routes->get('/company/:company_id/customer/:customer_id', ['controller' => 'Applications', 'action' => 'view'])
-        //     ->setPass(['company_id', 'companyId'], ['customer_id', 'customerId']);
-        
-        // $routes->post('/', ['controller' => 'Applications', 'action' => 'add']);
+        // Applications
+        $routes->post('/add', ['controller' => 'Applications', 'action' => 'add']);
+
+        $routes->post('/:application_id/edit/', ['controller' => 'Applications', 'action' => 'edit'])
+            ->setPass(['application_id']);
+
         // $routes->fallbacks(DashedRoute::class);
     }
 );
