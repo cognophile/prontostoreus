ec704b34510574fb76d6d6af04c59dd4623da9d3
cognophile <cognophile@users.noreply.github.com>
2018-07-02 23:25:56 +0100

    Add invoice database schema

---
+++ components/InvoiceComponent/config/Migrations/20180702203608_CreateInvoicesTable.php
@@ -0,0 +1,45 @@
+<?php
+use Migrations\AbstractMigration;
+use Phinx\Util\Literal;
+
+class CreateInvoicesTable extends AbstractMigration
+{
+    public $autoId = false;
+    
+    public function up()
+    {
+        $exists = $this->hasTable('invoices');
+
+        if (!$exists)
+        {
+            $table = $this->table('invoices');
+
+            $table->addColumn('id', 'integer', [
+                'autoIncrement' => true,
+            ])
+                ->addPrimaryKey('id')
+                ->addColumn('application_id', 'integer', ['null' => true, 'default' => null])
+                    ->addForeignKey('application_id', 'applications', 'id', ['delete' => 'NO_ACTION', 'update' => 'NO_ACTION'])
+                
+                ->addColumn('reference', 'string', ['null' => false, 'default' => null])
+                ->addColumn('subject', 'string', ['null' => false, 'default' => null])
+                ->addColumn('issued', 'datetime', ['null' => false, 'default' => 'CURRENT_TIMESTAMP'])
+                ->addColumn('due', 'datetime', ['null' => false, 'default' => null])
+                ->addColumn('paid', 'string', ['null' => false, 'default' => '0.00'])
+                ->addColumn('total', 'string', ['null' => false, 'default' => '0.00'])
+                ->addColumn('created', 'datetime', ['null' => false, 'default' => 'CURRENT_TIMESTAMP'])
+                ->addColumn('complete', 'boolean', ['null' => false, 'default' => 0]);
+
+            $table->create();
+        }
+    }
+
+    public function down()
+    {
+        if ($this->hasTable('invoices')) {
+            $this->execute("SET FOREIGN_KEY_CHECKS=0");
+            $this->dropTable('invoices');
+            $this->execute("SET FOREIGN_KEY_CHECKS=1");
+        }
+    }
+}
