8b3c0db43bae58b588d0aef29e71b5fb8909db15
cognophile <cognophile@users.noreply.github.com>
2018-07-05 00:48:07 +0100

    Update to create invoice from application on update

--- components/ApplicationComponent/src/Controller/ApplicationsController.php
+++ components/ApplicationComponent/src/Controller/ApplicationsController.php
@@ -15,6 +15,7 @@ class ApplicationsController extends AbstractApiController
     {
         parent::initialize();        
         $this->loadModel('ApplicationComponent.Applications');
+        $this->loadModel('InvoiceComponent.Invoices');
     }
 
     public function status()
@@ -30,6 +31,38 @@ class ApplicationsController extends AbstractApiController
 
     public function edit($applicationId) 
     {
+        if (!$applicationId) {
+            try {
+                throw new InvalidArgumentException('An application ID must be provided.');
+            }
+            catch (InvalidArgumentException $ex) {
+                Log::write('error', $ex->getErrors());
+                $this->response = $this->response->withStatus(400);
+                $this->respondError($ex->getErrors(), $this->messageHandler->retrieve("Error", "InvalidArgument"));
+            }
+        }
+
+        $isSuccessful = $this->createInvoice($this->request->getData());
         return $this->universalEdit($this->Applications, $applicationId);
+    }
+
+    private function createInvoice(array $applicationData)
+    {
+        if (!is_array($applicationData) || empty($applicationData)) {
+            throw new InvalidArgumentException('The provided data must be a valid array.');
+        }
+
+        $customer = $this->Applications->find('customerByApplicationId')->toArray();
+        $invoiceData = $this->Invoices->buildInvoiceData($applicationData, $customer['firstname'], $customer['surname']);
+        
+        $invoice = $this->Invoices->newEntity($invoiceData);
+        $newInvoice = $this->Invoices->saveEntity($this->Invoices, $invoice);
+
+        if ($newInvoice->getErrors()) {
+            Log::write('error', $entity->getErrors());
+            return false;
+        }
+
+        return true;
     }
 }
