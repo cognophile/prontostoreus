366dc2685b2464bff5e500eacfaf7940bd8741f7
cognophile <cognophile@users.noreply.github.com>
2018-07-28 16:25:51 +0100

    Update to ensure furnishing record associated with given room

--- components/ApplicationComponent/src/Controller/FurnishingsController.php
+++ components/ApplicationComponent/src/Controller/FurnishingsController.php
@@ -27,14 +27,32 @@ class FurnishingsController extends AbstractApiController
         }
 
         $this->loadModel('ApplicationComponent.Furnishings');
-        $results = $this->Furnishings->find('byRoomId', ['roomId' => $roomId]);
+        $results = $this->Furnishings->find('byRoomId', ['roomId' => $roomId])->toArray();
 
         $this->respondSuccess($results, 200, $this->messageHandler->retrieve("Data", "Found"));
     }
 
     public function fetchFurnishingSize($roomId, $furnishingId)
     {
+        try {
+            $this->requestFailWhenNot('GET');
+        }
+        catch (MethodNotAllowedException $ex) {
+            $this->respondException($ex, $this->messageHandler->retrieve("Error", "MethodNotAllowed"));
+            return;
+        }
+
         $this->loadModel('ApplicationComponent.Furnishings');
+        $results = $this->Furnishings->find('byRoomId', ['roomId' => $roomId])->enableHydration(false)->toArray();
+
+        $furnishing = array_search($furnishingId, array_column($results, 'id'));
+
+        if ($furnishing === false) {
+            $this->respondError('Requested furnishing not associated with requested room', 404, 
+                $this->messageHandler->retrieve("Data", "NotFound"));
+            return;
+        }
+
         return parent::universalView($this->Furnishings, $furnishingId);
     }
 }
