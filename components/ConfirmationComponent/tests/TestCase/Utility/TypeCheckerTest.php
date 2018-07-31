2e44c6755354b85a55f243dd8af07a4afd37901e
cognophile <cognophile@users.noreply.github.com>
2018-08-01 00:16:03 +0100

    Add tests for isNumeric function

---
+++ components/ConfirmationComponent/tests/TestCase/Utility/TypeCheckerTest.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace ConfirmationComponent\Test\TestCase\Utility;
+
+use Cake\TestSuite\TestCase;
+use ConfirmationComponent\Utility\TypeChecker;
+
+class TypeCheckerTest extends TestCase
+{
+    public $fixtures = [
+
+    ];
+
+    public function validNumericValuesProvider()
+    {
+        return [
+            'Numeric Zero' => ["0"],
+            'Numeric One' => ["1"],
+            'Numeric Multiple' => ["1234"],
+        ];
+    }
+
+    public function invalidNonNumericValuesProvider()
+    {
+        return [
+            'Character Upper Case' => ["A"],
+            'Character Lower Case' => ["a"],
+            'Character and Numeric' => ["A1"],
+            'Character Upper Case' => ["@"],
+            'Numeric Decimal' => ["1.25"],
+        ];
+    }
+
+    /**
+     * @dataProvider validNumericValuesProvider
+     */
+    public function testIsNumericWithNumericValuesReturnsTrue($value)
+    {
+        $actual = TypeChecker::isNumeric($value);
+        $this->assertTrue($actual);
+    }
+
+    /**
+     * @dataProvider invalidNonNumericValuesProvider
+     */
+    public function testIsNumericWithNonNumericValuesReturnsFalse($value)
+    {
+        $actual = TypeChecker::isNumeric($value);
+        $this->assertFalse($actual);
+    }
+}
